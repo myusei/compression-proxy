f18ca9bef0a15f75718b2f1ae1c6786bd897a8cb
yusei <yusei.m.v@gmail.com>
2020-01-29 04:31:26 +0900

    script edited

--- mitm_filter.py
+++ mitm_filter.py
@@ -1,51 +1,78 @@
 from PIL import Image
-import io, time, gzip
+import io, gzip, time
 import brotli
-path = '/var/log/mitmproxy/mitm_filter.log'
 
-def response(flow):
+def measure(func):
+    def _measure(*args, **kwargs):
+        path = '/var/log/mitm.log'
+        before = int(args[0].response.headers["content-length"])
+        start = time.time()
+        flow = func(args[0], **kwargs)
+        end = time.time()
+        after = int(flow.response.headers["content-length"])
+        ct = args[0].response.headers["content-type"]
+        ct = ct[0:10]
+        with open(path, mode='a') as f:
+            f.write(f'type:{ct}\tcompressed:{after / before :.2%}\treduced:{before - after}\tsource:{before}\ttime:{end - start:.5f}\n') 
+        return flow
+    return _measure
+
+def compress_gzip(flow):
+    content_binay = io.BytesIO()
+    gz = gzip.GzipFile(fileobj=content_binay, mode='w')
+    gz.write(flow.response.content)
+    gz.close()
+    flow.response.content = content_binary.getvalue()
+    flow.response.headers["content-encoding"] = "gzip"
+    return flow
+
+def compress_brotli(flow):
+    flow.response.content = brotli.compress(flow.response.content, quality=10)
+    flow.response.headers["content-encoding"] = "br"
+    return flow
+
+def compress_png(flow):
+    img = Image.open(io.BytesIO(flow.response.content))
+    if img.mode == "RGBA" or "transparency" in img.info:
+        content_binary = io.BytesIO()
+        img = img.convert(mode='P', palette=Image.ADAPTIVE)
+        #img.save(content_binary, "png", optimize = True, bits = 8)
+        img.save(content_binary, bits = 8)
+        flow.response.content = content_binary.getvalue()
+    else:
+        flow = compress_jpeg(flow)
+    return flow
+
+def compress_jpeg(flow):
+    content_binary = io.BytesIO()
+    img = Image.open(io.BytesIO(flow.response.content)).convert("RGB")
+    img.save(content_binary, "jpeg", quality = 5, optimize = True, progressive = True)
+    flow.response.headers["content-type"] = "image/jpeg"
+    flow.response.content = content_binary.getvalue()
+    return flow
+
+@measure
+def compress(flow):
     if "content-type" in flow.response.headers and "content-length" in flow.response.headers:
         ct = str(flow.response.headers["content-type"])
         cl = int(flow.response.headers["content-length"])
-        s = io.BytesIO(flow.response.content)
-        s2 = io.BytesIO()
-        if (cl) > 100:
-
-            # 画像をjpeg quality 10/100に変換する
-            if (ct) [0:6] == ("image/") and (cl) > 1000 and (ct) [0:9] != ("image/svg"):
-                start = time.time()
+        if (cl) > 256:
+            if (ct) [0:6] == ("image/") and (ct) [0:9] != ("image/svg"):
                 if (ct) [0:9] == ("image/png"):
-                    img = Image.open(s)
-                    if img.mode == "RGBA" or "transparency" in img.info:
-                        img.save(s2, "png", optimize = True, bits = 8)
-                    else:
-                        img = Image.open(s).convert("RGB")
-                        #img = Image.open(s).convert("L")
-                        img.save(s2, "jpeg", quality = 10, optimize = True, progressive = True)
-                        flow.response.headers["content-type"] = "image/jpeg"
+                    flow = compress_png(flow)
                 else:
-                    img = Image.open(s).convert("RGB")
-                    img.save(s2, "jpeg", quality = 10, optimize =  True, progressive = True)
-                    flow.response.headers["content-type"] = "image/jpeg"
-                flow.response.content = s2.getvalue()
-                return
-
-            # スキームが http のテキストを gzip 圧縮する
-            if flow.request.scheme == "http" and not "content-encoding" in flow.response.headers:
+                    flow = compress_jpeg(flow)
+            elif flow.request.scheme == "http" and not "content-encoding" in flow.response.headers:
                 flow.response.headers["content-encoding"] = "none"     
                 if (ct) [0:5] == ("text/") or (ct) [0:12] == ("application/") or (ct) [0:9] == ("image/svg"):
-                    gz = gzip.GzipFile(fileobj=s2, mode='w')
-                    gz.write(flow.response.content)
-                    gz.close()
-                    flow.response.content = s2.getvalue()
-                    flow.response.headers["content-encoding"] = "gzip"
-            # スキームが https のテキストを brotli 圧縮する
+                    flow = compress_gzip(flow)
             elif flow.request.scheme == "https" and not "content-encoding" in flow.response.headers:
                 flow.response.headers["content-encoding"] = "none"
-                if (ct) [0:5] == ("text/") and (ct) [0:10] != ("text/plain") and (ct) [0:9] != ("text/html") or (ct) [0:12] == ("application/") and (ct) [0:16] != ("application/json") or (ct) [0:9] == ("image/svg"):
-                    s2 = flow.response.content
-                    s3 = brotli.compress(s2, quality=10)
-                    flow.response.content = s3
-                    flow.response.headers["content-encoding"] = "br"
-                
-                
+                #if (ct) [0:5] == ("text/") and (ct) [0:10] != ("text/plain") and (ct) [0:9] != ("text/html") or (ct) [0:12] == ("application/") and (ct) [0:16] != ("application/json") or (ct) [0:9] == ("image/svg"):
+                if (ct) [0:5] == ("text/") or (ct) [0:12] == ("application/") or (ct) [0:9] == ("image/svg"): 
+                    flow = compress_brotli(flow)
+    return flow
+
+def response(flow):
+    flow = compress(flow)
+
